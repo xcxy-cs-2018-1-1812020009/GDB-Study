b312f19f63d7e71f4859cc44c3c7cf19c6c55f1c
xcxy-cs-2018-1-1812020009 <1229500840@qq.com>
2020-12-10 12:45:03 +0800

    GDB常用命令练习

---
+++ test2/demo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* 函数声明 */
+int max(int num1, int num2);
+int main ()
+{
+   /* 局部变量定义 */
+   int a,b,ret;
+   scanf("%d",&a);
+   scanf("%d",&b);
+   /* 调用函数来获取最大值 */
+   ret = max(a, b);
+   printf( "Max value is : %d\n", ret );
+   return 0;
+}
+/* 函数返回两个数中较大的那个数 */
+int max(int num1, int num2)
+{
+   /* 局部变量声明 */
+   int result;
+   if (num1 > num2)
+      result = num1;
+   else
+      result = num2;
+   return result;
+}
---
+++ test4/demo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int a=0;
+    for(int i=0; i<10; i++)
+        a+=i;
+}
